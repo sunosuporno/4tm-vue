ad38966dd7fe1cbbd41c7808ddd77d07dcd8f698
sunosuporno <sarkarsuporno36@gmail.com>
2022-02-28 17:50:31 +0530

    added twitter and icon

---
+++ src/composables/qna.js
@@ -0,0 +1,35 @@
+export const questions = [
+    {
+      id: "faq_question_1",
+      question:
+        "What happens when all tokens get sold and still more people want to get in?",
+      answer: "Can always increase the number of pixels.",
+    },
+    {
+      id: "faq_question_4",
+      question:
+        "Why you ain’t launching on Ethereum? That’s the place to be, isn’t it?",
+      answer:
+        "Love Ethereum ❤️. But, we’d rather accept Mastercard than make people pay over $50 equivalent of ETH for just sending a txn.",
+    },
+    {
+      id: "faq_question_5",
+      question:
+        "Are you actually selling 100,000 tokens fr? Or would you reserve some?",
+      answer:
+        "We will reserve some. Probably around 100. A bunch will be distributed to get the word out. Majority will be reserved for inviting people from other walks of life, who are still not fully into crypto.",
+    },
+    {
+      id: "faq_question_6",
+      question: "Where will be the metadata stored related to each pixel?",
+      answer:
+        "Some data will be stored on the chain. Rest off chain in IPFS. A copy of just the pixel data will be stored in a centralized database to build the canvas. This design has been chosen so that the pixel can be edited by owners as much as they want without pushing gas prices too high.",
+    },
+    {
+      id: "faq_question_7",
+      question:
+        "Gotta address the elephant in the room now. What benefits do I get to be a part of this?",
+      answer:
+        "Along with everything said on the roadmap, you get complete control and right over the pixel you mint. You decide the color and quote on it. Let’s say you have a way with your words, and we want to print your quote on the merch, you can ask for royalties. Apart from that, anything you’re working on, you’re free to showcase that in front of everyone and also collaborate with anyone in the community.",
+    },
+  ];
